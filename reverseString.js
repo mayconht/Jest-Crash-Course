f8a350527ee221000d523ba8dd0e9b7a36baa2bf
Maycon Douglas <maycondss@live.com>
2023-03-10 17:00:18 -0300

    Reverse test  -

---
+++ reverseString.js
@@ -0,0 +1,9 @@
+const reverseString = (str) => {
+    return str
+    .split('')
+    .reverse()
+    .join('')
+    .toLowerCase();
+    }
+
+module.exports = reverseString;
---
+++ reverseString.test.js
@@ -0,0 +1,12 @@
+const reverseString = require('./reverseString');
+
+test('ReverseString function exists', () =>{
+    expect(reverseString).toBeDefined();
+})
+
+test('String reverses', () =>{
+    expect(reverseString('hello')).toEqual('olleh');
+})
+test('String reverses with uppercase', () =>{
+    expect(reverseString('Hello')).toEqual('olleh');
+})
